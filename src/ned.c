d5f86f596906c059921f6135bf39f25cf9ffe66d
Matthew Davies <mattie.davies@gmail.com>
2019-08-09 09:27:03 -0400

    Some refactoring.

--- src/ned.c
+++ src/ned.c
@@ -14,18 +14,22 @@ extern u8* testFile;
 
 int main()
 {
+    u8 handle;
+    u32 p1, p2;
+    u8 *a1, *a2;
+
     initVideo();
 
-    u8 handle = arenaNew();
+    handle = arenaNew();
     BREAK;
-    u32 p1 = arenaAlloc(handle, 8000);
-    u32 p2 = arenaAlloc(handle, 8000);
+    p1 = arenaAlloc(handle, 8000);
+    p2 = arenaAlloc(handle, 8000);
 
     BREAK;
-    u8* a1 = arenaPrepare(p1);
+    a1 = arenaPrepare(p1);
     display16(0, 0, (u16)a1, 0);
     BREAK;
-    u8* a2 = arenaPrepare(p2);
+    a2 = arenaPrepare(p2);
     display16(0, 1, (u16)a2, 0);
     BREAK;
 
